016da79d5036c33931cd1dc023a3639e73d53f99
julienthevenard <thevenard.julien@gmail.com>
2025-07-11 15:33:37 +0200

    more corrections

--- components/TodoList-interactive.tsx
+++ components/TodoList-interactive.tsx
@@ -1,24 +1,24 @@
 "use client";
 
-import { Todo } from "@/types/todos";
+import { Todos } from "@/types/todos";
 import { useState } from "react";
 import { Eye, Trash2 } from "@deemlol/next-icons";
 import Link from "next/link";
 
-export default function TodoList({ todos }: { todos: Todo[] }) {
-  const [todoList, setTodoList] = useState<Todo[]>(todos);
+export default function TodoList({ todos }: { todos: Todos }) {
+  const [todoList, setTodoList] = useState<Todos>(todos);
 
   const handleDelete = (id: number) => {
-    setTodoList(todoList.filter((todo) => todo.id !== id));
+    setTodoList(todoList.filter((todo) => todo && todo.id !== id));
   };
 
   return (
     <ul className="list-disc flex flex-col gap-4">
-      {todoList.map((todo) => (
-        <li className="flex items-center gap-4" key={todo.id}>
-          <p>{todo.title}</p>
+      {todoList && todoList.map((todo) => (
+        <li className="flex items-center gap-4" key={todo && todo.id}>
+          <p>{todo && todo.title}</p>
           <div className="flex gap-2 items-center">
-            <Link href={`/tasks/task/${todo.id}`}>
+            <Link href={`/tasks/task/${todo && todo.id}`}>
               <Eye
                 size={24}
                 color={`var(--color-neutral-700)`}
@@ -27,7 +27,7 @@ export default function TodoList({ todos }: { todos: Todo[] }) {
             </Link>
             <button
               className="rounded-md text-white px-2 py-1  transition-all duration-300 hover:scale-105 hover:cursor-pointer"
-              onClick={() => handleDelete(todo.id)}
+              onClick={() => handleDelete(todo && todo.id || 0)}
             >
               <Trash2 size={24} color={`var(--color-neutral-700)`} />
             </button>
--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,16 +1,16 @@
-import { Todo } from "@/types/todos";
+import { Todos } from "@/types/todos";
 import { Eye } from "@deemlol/next-icons";
 import Link from "next/link";
 
-export default function TodoList({ todos }: { todos: Todo[] }) {
+export default function TodoList({ todos }: { todos: Todos }) {
 
   return (
     <ul className="list-disc flex flex-col gap-4">
-      {todos.map((todo) => (
-        <li className="flex items-center gap-4" key={todo.id}>
-          <p>{todo.title}</p>
+      {todos && todos.map((todo) => (
+        <li className="flex items-center gap-4" key={todo && todo.id}>
+          <p>{todo && todo.title}</p>
           <div className="flex gap-2 items-center">
-            <Link href={`/tasks/task/${todo.id}`}>
+            <Link href={`/tasks/task/${todo && todo.id}`}>
               <Eye
                 size={24}
                 color={`var(--color-neutral-700)`}
